6f80c2e00a2c6a8ad9aeb607f635bf2da2901441
Your Name <you@example.com>
2021-11-23 16:55:41 +0300

    initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,31 @@
+
+$( function() {
+    $('#window_hak').draggable();
+    $('#window_hak').resizable();
+} )
+$( function() {
+    $('#window_tg').draggable();
+    $('#window_tg').resizable();
+} )
+$( function() {
+    $('#window_ds').draggable();
+    $('#window_ds').resizable();
+} )
+$( function() {
+    $('#window_wp').draggable();
+    $('#window_wp').resizable();
+} )
+$( function() {
+    $('#window_yt').draggable();
+    $('#window_yt').resizable();
+} )
+
+$( function() {
+    $('.tg').load("https://webz.telegram.org body");
+    $('#window_yt').resizable();
+} )
+
+
+
+
+
